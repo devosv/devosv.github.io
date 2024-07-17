50f26c8568f5fe08e6d1bb3470db674a013769b6
devosv <funing.bili@msn.com>
2024-07-17 18:19:12 +0800

    Site updated: 2024-07-17 18:19:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/17/hello-world/"];function toRandomPost(){
+var posts=["p/clypowxa3000378uqb4fue1rx.html","p/clypowxa0000178uq5ktzetir.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
